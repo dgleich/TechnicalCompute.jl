c4a4b89b8c79115ffeaf79510c9cd8f9d555b28b
David Gleich <dgleich@purdue.edu>
2019-09-13 05:38:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+TechnicalCompute.jl
+===================
+
+This package is a meta-package for a wide variety of commonly used Julia packages. It is
+designed to make it easy to include one package that handles the diversity of technical
+computing requirements for an undergraduate degree.
+
---
+++ src/TechnicalCompute.jl
@@ -0,0 +1,43 @@
+module TechnicalCompute
+
+using Reexport 
+
+@reexport using SparseArrays
+@reexport using Random
+@reexport using FFTW # need 
+@reexport using Combinatorics 
+@reexport using LinearAlgebra
+@reexport using DelimitedFiles
+@reexport using Printf
+@reexport using Statistics
+@reexport using StatsBase
+@reexport using DataStructures
+@reexport using KahanSummation
+@reexport using BenchmarkTools
+@reexport using SpecialFunctions
+@reexport using FileIO
+@reexport using IterTools
+@reexport using Polynomials
+@reexport using TaylorSeries
+@reexport using TestImages
+
+@reexport using StaticArrays
+@reexport using JSON
+@reexport using DSP
+@reexport using Roots 
+@reexport using JuMP 
+@reexport using Plots
+@reexport using MultivariateStats
+@reexport using DataFrames
+@reexport using CSV
+@reexport using DifferentialEquations 
+@reexport using HDF5
+@reexport using Images
+@reexport using LightGraphs
+@reexport using SuiteSparse
+@reexport using Arpack
+@reexport using Flux 
+@reexport using NearestNeighbors
+@reexport using QuadGK
+
+end # module
