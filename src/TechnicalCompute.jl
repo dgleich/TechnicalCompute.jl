01af7efebac3434c5411a561eb0b8025bece87d1
David Gleich <dgleich@purdue.edu>
2024-10-03 16:12:40 -0400

    Updates

--- src/TechnicalCompute.jl
+++ src/TechnicalCompute.jl
@@ -109,12 +109,46 @@ packages = [
 "Ipopt",
 "GLPK",
 "Clp",
+"HiGHS",
 # diff Equations
 "DifferentialEquations"
 ]
 
+# handle Clp failures
+if Sys.ARCH == :aarch64 && Sys.isapple() 
+  filter!(x -> x != "Clp", packages)
+end 
+
 for pkg in packages
   eval(Meta.parse("@reexport using $pkg"))
 end 
+
+const overrides = Set{Symbol}()
+
+update!(p::ProgressMeter.AbstractProgress, val, color; options...) = ProgressMeter.update!(p, val, color; options...)
+update!(p::Union{ProgressMeter.Progress,ProgressMeter.ProgressUnknown,ProgressMeter.ProgressThresh}, val; options...) = ProgressMeter.update!(p, val; options...)
+update!(p::Union{ProgressMeter.Progress,ProgressMeter.ProgressUnknown,ProgressMeter.ProgressThresh}; options...) = ProgressMeter.update!(p; options...)
+update!(h::DataStructures.MutableBinaryHeap, i, v) = DataStructures.update!(h, i, v)
+#update!(points, bt::Delaunator.BasicTriangulation, cdata::Delaunator.TriangulationTemporaries; tol) = Delaunator.update!(points, bt, cdata; tol)
+export update!
+push!(overrides, :update!)
+
+# This ignores images, which also exports it's function under tensor. 
+⊗(k::Integer) = LinearMaps.⊗(k)
+⊗(A,B,Cs...) = LinearMaps.⊗(A,B,Cs...)
+⊗(x::T, y::T) where T<:Union{Float16, Float32, Float64} = DoubleFloats.⊗(x, y)
+export ⊗
+push!(overrides, :⊗)
+
+@doc (@doc Meshes.partition)
+partition(rng::AbstractRNG, object, method::Meshes.PartitionMethod) = Meshes.partition(rng, object, method)
+partition(object, method::Meshes.PartitionMethod) = Meshes.partition(object, method)
+
+@doc (@doc IterTools.partition)
+partition(xs, n::Int64) = IterTools.partition(xs, n)
+partition(xs::I, n::Int64, step::Int64) where I = IterTools.partition(xs, n, step)
+export partition
+push!(overrides, :partition)
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,53 @@ using Test
 # check for method duplicates in the libraries of Technical compute and 
 # make sure we export a method from Technical compute with the same name
 
+function _names_in_packages(pkgs)
+  namesused = Dict{Symbol, Vector{Module}}()
+  for pkg in pkgs
+      for name in names(pkg)
+          if haskey(namesused, name)
+              push!(namesused[name], pkg)
+          else
+              namesused[name] = [pkg]
+          end
+      end
+  end
+  return namesused
+end
+function test_name_in_packages_for_equality(packages, name, mynames)
+  if name == :gamma
+    filter!(p -> p !== Combinatorics, packages)
+  end 
+  handles = map(packages) do pkg
+    getfield(pkg, name)
+  end
+  for i in eachindex(handles)
+    for j in i+1:length(handles)
+      if handles[i] !== handles[j]
+        #println("Name $name is not equal in packages $(packages[i]) and $(packages[j]) and $(name in TechnicalCompute.overrides)")
+        if name in TechnicalCompute.overrides
+          # no error, we've handled it...
+        else
+          @test_broken name in TechnicalCompute.overrides 
+          println("Name $name is not equal in packages $(packages[i]) and $(packages[j]) and not in overrides")
+        end
+      end
+    end
+  end
+end 
+function test_names_in_packages_for_override(pkgs)
+  namesused = _names_in_packages(map(pkg->eval(Meta.parse("$pkg")), pkgs))
+  mynames = names(TechnicalCompute)
+  for (name, pkgs) in namesused
+    if length(pkgs) > 1
+      test_name_in_packages_for_equality(pkgs, name, mynames)
+    end
+  end
+end
+
+@testset "duplicate names and overrides" begin 
+  test_names_in_packages_for_override(TechnicalCompute.packages)
+end 
 
 @testset "Random" begin
   Random.seed!(0)
@@ -22,11 +69,8 @@ end
   @test String(nthperm(Vector{Char}("abc"), 2)) == "acb"
 end
 
-# make sure we can solve a simple LP
-@testset "Optimization" begin
-
-  # https://discourse.julialang.org/t/writing-a-simple-production-scheduling-optimal-control-problem-in-jump/17748
-  m = Model(GLPK.Optimizer)
+function _test_optimal_horizon(m) 
+  # # https://discourse.julialang.org/t/writing-a-simple-production-scheduling-optimal-control-problem-in-jump/17748
   T = 4 # 4 weeks horizon
 
   @expression(m, g[t=0:T], -t+5)
@@ -41,27 +85,10 @@ end
   optimize!(m)
 
   @test termination_status(m) != JuMP.MOI.TerminationStatusCode(1)
+end 
 
-  # Try Clp
-  m = Model(Clp.Optimizer)
-  T = 4 # 4 weeks horizon
+function _test_nonlinear(model)
 
-  @expression(m, g[t=0:T], -t+5)
-
-  @variable(m, 0 <= x[0:T] <= 0.8)
-  @variable(m, 0 <= y[0:T] <= 0.9, start=0.0)
-
-  @objective(m, Min, 2*sum(x) + 3*sum(y))
-
-  @constraint(m, [t in 0:(T-1)], y[t+1] == y[t] + (x[t+1] - g[t+1]))
-
-  optimize!(m)
-
-  @test termination_status(m) != JuMP.MOI.TerminationStatusCode(1)
-  
-  # Ipopt
-  model = Model(Ipopt.Optimizer)
-  
   @variable(model, x, start = 0.0)
   @variable(model, y, start = 0.0)
 
@@ -69,11 +96,36 @@ end
 
   optimize!(model)
 
+  @test termination_status(model) != JuMP.MOI.TerminationStatusCode(1)
+
   # adding a (linear) constraint
   @constraint(model, x + y == 10)
   optimize!(model)
 
-  @test true # just make sure we get here
+  # only test that we get here...
+  @test true 
+  #@test termination_status(model) != JuMP.MOI.TerminationStatusCode(1)
+end 
+
+# make sure we can solve a simple LP
+@testset "Optimization" begin
+
+  _test_optimal_horizon(Model(GLPK.Optimizer))
+
+  # Try Clp 
+  # broken on aarch64
+  # https://github.com/jump-dev/Clp.jl/issues/131
+  if Sys.ARCH == :aarch64 && Sys.isapple() 
+    @test_broken false
+  else 
+    _test_optimal_horizon(Model(Clp.Optimizer))
+  end 
   
-  
+  # # Ipopt
+  _test_nonlinear(Model(Ipopt.Optimizer))
+  _test_optimal_horizon(Model(Ipopt.Optimizer))
+
+  # HiGHS
+  _test_optimal_horizon(Model(HiGHS.Optimizer))
+
 end
